7a739a00f45f72d1b28f1a7e4ebf865aa1538c79
Nilesh Kumar <nileshkumar0613@gmail.com>
2026-01-10 16:44:56 +0530

    Fix backend URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,5 +4,14 @@ const api = axios.create({
   baseURL: "https://virtual-trading-app-hnd6.onrender.com",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+});
+
 export default api;
+
 
