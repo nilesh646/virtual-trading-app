a08127937b0d4eb8fb327d793ff108f126cea4ac
Nilesh Kumar <nileshkumar0613@gmail.com>
2026-01-16 15:14:08 +0530

    Fix missing toast import in Trade component

--- client/src/components/Trade.js
+++ client/src/components/Trade.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import api from "../api/axios";
+import toast from "react-hot-toast"; 
 
 const Trade = ({ refreshWallet }) => {
   const [symbol, setSymbol] = useState("");
